89748bcbedb8b9bc871db7dc20ed8ba73ab8740e
Zeyad Hegazy <zhegazy409@gmail.com>
2024-08-16 07:27:50 +0300

    Update Diplomas.jsx
    add in diploma card total hours and chapters length and add chapter button, and create button to dispatch create diploma action

--- src/pages/admin-view/Diplomas.jsx
+++ src/pages/admin-view/Diplomas.jsx
@@ -1,4 +1,4 @@
-import { Card, Col, Row } from "react-bootstrap";
+import { Button, Card, Col, Row } from "react-bootstrap";
 import Pageheader from "../../layout/layoutcomponent/pageheader";
 import { useSelector } from "react-redux";
 
@@ -9,17 +9,34 @@ const Diplomas = () => {
 		<div className="m-4 position-relative">
 			<Pageheader title="Diplomas" heading="Main Menu" active="Diplomas" />
 
+			<div className="mb-4">
+				<Button>Add Diploma</Button>
+			</div>
+
 			{diplomas?.length > 0 ? (
 				diplomas.map((diploma) => (
 					<Row className="row" key={diploma._id}>
 						<Col sm={4}>
 							<Card className={`card-primary`}>
-								<Card.Header className=" pb-0">
-									<h5 className="card-title mb-0 pb-0">{diplomas.title}</h5>
+								<Card.Header className="pb-0 d-flex">
+									<h5 className="card-title mb-0 pb-0">{diploma.title}</h5>
 								</Card.Header>
 								<Card.Body className={`text-primary`}>
 									{diploma.description}
 								</Card.Body>
+								<Card.Footer className="d-flex justify-content-between">
+									<h6>
+										Total Hours :
+										<span className="text-primary">${diploma.totalHours}</span>
+									</h6>
+									<h6>
+										Chapters :
+										<span className="text-primary">{diploma.chapters}</span>
+									</h6>
+								</Card.Footer>
+								<div className="m-2">
+									<Button>Add Chapter</Button>
+								</div>
 							</Card>
 						</Col>
 					</Row>
@@ -27,7 +44,7 @@ const Diplomas = () => {
 			) : (
 				<Card>
 					<Card.Body>
-						<p className="text-center">No data available</p>
+						<p className="text-center text-primary">No data available</p>
 					</Card.Body>
 				</Card>
 			)}
